f2ca3fd00307e65cc2fcac9b843f3678e8fc398f
木森焱 <wangwenxiao03@163.com>
2018-08-22 14:30:07 +0800

    Update index.js
    这是一个嵌套循环和递归组合查找所需数据的一个算法。该算法并不高深，也没有什么创新，只是对迭代递归进行了一些修改。性能上作者并没有测试过，理论上讲要比传统递归、迭代递归性能上要好一点。
    嵌套循环递归对循环的数据结构有限制，必须是数组和对象组合形式的数据结构才能使用该算法，这也是改方式的一个局限。

--- Algorithm/NestingLoopRecursion/js/index.js
+++ Algorithm/NestingLoopRecursion/js/index.js
@@ -1,1 +1,28 @@
-dd
+/**
+      * @method recursionQuery 递归查询
+      * @author msy
+      * @param {object} component  需要递归的目标数据
+      * @param {string} name 条件名称 
+      * @return {object} 需要的数据
+     */
+    function recursionQuery(component,name){
+        //递归查找
+        function query(components,parents){
+            let _arrayList = [],_parents = [],j = 0,u = 0;
+
+            for(let i = 0,len = components.length;i < len;i++)
+            {
+                const e = components[i];
+                //如果控件是容器则合并到数组中，如果本次循环未查找到按钮，则在下次循环中循环该数组。
+                if(e.components){ _arrayList = _arrayList.concat(e.components); _parents.push(e); }
+                //更改存放父级的数组，保证获取到按钮的直系父级
+                if(u != parents[j].components.length){ u++; }else{ j++; u = 0;  }
+                //如果查找到按钮返回，并停止递归
+                if(e.lightboxBtn == name){ return {component:e,parent:parents[j]}  }
+            }
+            //如果数组中无元素则不再递归
+            if(_arrayList.length){ return query(_arrayList,_parents); }
+        }
+        
+        return query(component.components,[component])
+    }
